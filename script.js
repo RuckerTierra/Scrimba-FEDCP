9e65643459bc19c8198988b1833ed1722cd871cf
Tierra <50035689+RuckerTierra@users.noreply.github.com>
2020-10-13 23:26:36 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,46 +1,43 @@
-//loops and arrays part 2
+//loops and arrays part 3
 
-//Challenge: you are working at a very fancy new nightclub and in charge of letting people in based on their age. If they are not 21 years of age, you cannot let them in. Please send them a nice personalised message letting them know.
+//Challenge: Build a pac-man board. A pacman board is a board 18 x 18 squares large that has squares 
+//coloured based on if there is a wall in the square, a power-pellet in the square, a pac-dot in the
+//square, pac-man himself, or the four ghosts, inky, blinky, pinky and clyde. Based on the game //board example, and what you have learned about if statements and adding classes in javascript, I //would like you to create your own pac-man board. Be as creative with the maze as you like.
 
-//Dont forget to share your code on the #share-your-code channel on discord.
+//I have gone ahead an added 324 squares for you in the html and made an array for you to work //with.
 
-const nightClubRegister = [
-    {
-        name: 'Ahmed',
-        lastname: 'Abdool',
-        age: 25,
-        gender: 'male'
-    },
-    {
-        name: 'Sally',
-        lastname: 'Morgan',
-        age: 18,
-        gender: 'female'
-    },
-    {
-        name: 'Dionne',
-        lastname: 'Brown',
-        age: 29,
-        gender: 'female'
-    },
-    {
-        name: 'Max',
-        lastname: 'Forrester',
-        age: 20,
-        gender: 'male'
-    }
+//please dont forget to share your code on the #share-your--code channel on discord.
+
+const squares = Array.from(document.querySelectorAll('.grid div'))
+let layout = [
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 2, 2, 2, 1, 1, 1, 1, 1, 1, 2, 2, 2, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 2, 2, 2, 1, 1, 1, 1, 1, 1, 2, 2, 2, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
 ]
+// Goal: create a pac man board 
+// loop over another array of icons (pac man, dots, ghosts, etc.) on top of the squares array 
+// layout 
+// step 1: add class of wall to number 1
 
-for (let i = 0; i < nightClubRegister.length; i++){
-    if (nightClubRegister[i].age < 21){
-        if (nightClubRegister[i].gender === "female"){
-            console.log("I'm sorry, " + nightClubRegister[i].name + " You must be 21, ma'am.");
-        } else {
-        console.log("I'm sorry, " + nightClubRegister[i].name +  ". You must be 21 to enter, sir.");
-        }
-    } else if (nightClubRegister[i].age >= 21 && nightClubRegister[i].gender === "female"){
-        console.log("Ladies first. " + nightClubRegister[i].name + ", welcome to the party, ma'am.");
-    } else {
-        console.log(nightClubRegister[i].name + ", welcome to the party, sir.");
-    }
+let wall = document.getElementsByName("grid div");
+
+for (let i = 0; i < layout.length; i++){
+    if (layout[i] === 1){ // step 1
+        let wall = document.getElementsByName("grid div").classList.add("wall");
+    } 
 }
